7fcf8dc23bacfa903549bd3377a9190a9d3964b4
tajyunn.comic@gmail.com <tajyunn.comic@gmail.com>
2022-04-12 12:56:56 +0900

    4/11

--- untitled0.py
+++ untitled0.py
@@ -1556,4 +1556,74 @@ Out[11]: b'5'
 f.seek(-3, 2)  # Go to the 3rd byte before the end
 Out[12]: 37
 
-#aaa
+#4/11
+
+
+
+while True:
+...     try:
+...         x = int(input("Please enter a number: "))
+...         break
+...     except ValueError:
+...         print("Oops!  That was no valid number.  Try again...")
+...
+
+
+Please enter a number: ｋ
+Oops!  That was no valid number.  Try again...
+
+Please enter a number: 32
+
+class B(Exception):
+    pass
+    
+
+class C(B):
+    pass
+    
+
+class D(C):
+    pass
+    
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+        
+B
+C
+D
+
+#B>C>DでD,CからBには戻れない感じ
+
+import sys
+
+try:
+    f = open('workfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print("OS error: {0}".format(err))
+except ValueError:
+    print("Could not convert data to an integer.")
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+    
+Could not convert data to an integer.
+
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+        f.close()
+        
